1248be6d067fee1f7796050c55d1adfcb98fa0c2
世界 <i@nekox.me>
2020-03-08 11:02:00 +0800

    喵

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+apt-get install -y git make zlib1g-dev libssl-dev gperf cmake default-jdk clang libc++-dev libc++abi-dev
+
+cd /td
+
+git submodule init && git submodule update
+
+cd td
+
+git fetch origin master
+git checkout origin/master
+git clean -fdx
+
+mkdir build && cd build
+
+export CXXFLAGS="-stdlib=libc++"
+
+CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX:PATH=.. \
+  -DTD_ENABLE_JNI=ON .. || exit
+
+cmake --build . --target install || exit
+
+cd ../..
+
+mkdir build && cd build
+
+CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX:PATH=../td/tdlib \
+  -DTd_DIR:PATH="$(readlink -e ../td/lib/cmake/Td)" .. || exit
+
+cmake --build . --target install || exit
+
+cd ..
+
+mv build/libtdjni.so .
+
+rm -rf build
